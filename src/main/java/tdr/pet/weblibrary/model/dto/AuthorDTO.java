4a9a7efb3475bd0ad46d81187d79b6f712e3ee7e
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-08-24 14:05:36 +0300

    added DTOs

---
+++ src/main/java/tdr/pet/weblibrary/model/dto/AuthorDTO.java
@@ -0,0 +1,14 @@
+package tdr.pet.weblibrary.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorDTO {
+    private String name;
+}
---
+++ src/main/java/tdr/pet/weblibrary/model/dto/PublisherDTO.java
@@ -0,0 +1,15 @@
+package tdr.pet.weblibrary.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PublisherDTO {
+    private String name;
+    private String address;
+}
