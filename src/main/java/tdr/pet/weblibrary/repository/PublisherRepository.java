24f0e5b02d74f3bdd182120a4403eba7dfda2392
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-08-25 19:26:22 +0300

    created publisher repository

--- src/main/java/tdr/pet/weblibrary/repository/PublisherRepository.java
+++ src/main/java/tdr/pet/weblibrary/repository/PublisherRepository.java
@@ -5,12 +5,19 @@ import org.springframework.stereotype.Repository;
 import tdr.pet.weblibrary.model.entity.Publisher;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-    List<Publisher> getPublishersByName(String name);
+    Optional<Publisher> getPublisherByName(String name);
 
     List<Publisher> getPublishersByAddress(String address);
 
-    List<Publisher> getPublisherByNameAndAddress(String name, String address);
+    Optional<Publisher> getPublisherByNameAndAddress(String name, String address);
+
+    Publisher updatePublisherById(Long id, Publisher publisher);
+
+    void createPublisher(Publisher publisher);
+
+    void deletePublisherById(Long id);
 }
