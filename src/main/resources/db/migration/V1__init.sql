4f47810308c6f6af73eb20771a99d3c3c44e1d0e
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-08-25 13:26:10 +0300

    fixed migrations inside container

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS authors
+(
+    id           BIGINT NOT NULL,
+    name         VARCHAR(255),
+    email        VARCHAR(255),
+    imgUrl       VARCHAR(255),
+    created      TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    modified     TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    CONSTRAINT pk_authors PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS publishers
+(
+    id           BIGINT NOT NULL,
+    name         VARCHAR(255),
+    address      VARCHAR(255),
+    created      TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    modified     TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    CONSTRAINT pk_publishers PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS books
+(
+    id           BIGINT  NOT NULL,
+    title        VARCHAR(255),
+    description  VARCHAR(255),
+    imgUrl       VARCHAR(255),
+    pages        INTEGER NOT NULL,
+    isbn         VARCHAR(255),
+    publisher_id BIGINT,
+    genre        SMALLINT,
+    written      TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    removed      BOOLEAN NOT NULL,
+    created      TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    modified     TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    CONSTRAINT pk_books PRIMARY KEY (id)
+);
+
+ALTER TABLE books
+    ADD CONSTRAINT fk_books_on_publisher FOREIGN KEY (publisher_id) REFERENCES publishers (id);
+
+
+CREATE TABLE book_authors
+(
+    authors_id BIGINT NOT NULL,
+    book_id    BIGINT NOT NULL,
+    CONSTRAINT pk_book_authors PRIMARY KEY (authors_id, book_id)
+);
+
+ALTER TABLE book_authors
+    ADD CONSTRAINT fk_books_and_authors_on_author FOREIGN KEY (authors_id) REFERENCES authors (id);
+
+ALTER TABLE book_authors
+    ADD CONSTRAINT fk_books_and_authors_on_book FOREIGN KEY (book_id) REFERENCES books (id);
