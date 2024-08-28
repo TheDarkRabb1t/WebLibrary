accf62f8300c242d07291ac7b7702dceb399a110
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-08-28 23:19:52 +0300

    added .sql constraints for fields

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,21 +1,21 @@
 CREATE TABLE IF NOT EXISTS authors
 (
-    id           BIGINT NOT NULL,
-    name         VARCHAR(255),
-    email        VARCHAR(255),
-    imgUrl       VARCHAR(255),
-    created      TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    modified     TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    id       BIGINT NOT NULL,
+    name     VARCHAR(255),
+    email    VARCHAR(255),
+    imgUrl   VARCHAR(255),
+    created  TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    modified TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     CONSTRAINT pk_authors PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS publishers
 (
-    id           BIGINT NOT NULL,
-    name         VARCHAR(255),
-    address      VARCHAR(255),
-    created      TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
-    modified     TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    id       BIGINT NOT NULL,
+    name     VARCHAR(255),
+    address  VARCHAR(255),
+    created  TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    modified TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
     CONSTRAINT pk_publishers PRIMARY KEY (id)
 );
 
@@ -36,16 +36,20 @@ CREATE TABLE IF NOT EXISTS books
     CONSTRAINT pk_books PRIMARY KEY (id)
 );
 
-ALTER TABLE books
-    ADD CONSTRAINT fk_books_on_publisher FOREIGN KEY (publisher_id) REFERENCES publishers (id);
-
-
 CREATE TABLE book_authors
 (
     authors_id BIGINT NOT NULL,
     book_id    BIGINT NOT NULL,
     CONSTRAINT pk_book_authors PRIMARY KEY (authors_id, book_id)
 );
+ALTER TABLE books
+    ADD CONSTRAINT fk_books_on_publisher FOREIGN KEY (publisher_id) REFERENCES publishers (id);
+ALTER TABLE books
+    ADD CONSTRAINT uq_book_isbn UNIQUE (isbn);
+ALTER TABLE authors
+    ADD CONSTRAINT uq_author_email UNIQUE (email);
+ALTER TABLE publishers
+    ADD CONSTRAINT uq_publisher_name UNIQUE (name);
 
 ALTER TABLE book_authors
     ADD CONSTRAINT fk_books_and_authors_on_author FOREIGN KEY (authors_id) REFERENCES authors (id);
