917ad5a07a814d648522c7086a07bfa1bb67b8a6
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-09-01 16:58:03 +0300

    reimpl new book rest controller

--- src/main/java/tdr/pet/weblibrary/controller/RestBookController.java
+++ src/main/java/tdr/pet/weblibrary/controller/RestBookController.java
@@ -2,18 +2,25 @@ package tdr.pet.weblibrary.controller;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import tdr.pet.weblibrary.model.dto.AuthorDTO;
 import tdr.pet.weblibrary.model.dto.BookDTO;
+import tdr.pet.weblibrary.model.dto.PublisherDTO;
 import tdr.pet.weblibrary.model.entity.Author;
 import tdr.pet.weblibrary.model.entity.Book;
 import tdr.pet.weblibrary.model.entity.Publisher;
+import tdr.pet.weblibrary.model.mapper.AuthorMapper;
 import tdr.pet.weblibrary.model.mapper.BookMapper;
+import tdr.pet.weblibrary.model.mapper.PublisherMapper;
 import tdr.pet.weblibrary.service.AuthorService;
 import tdr.pet.weblibrary.service.BookService;
 import tdr.pet.weblibrary.service.PublisherService;
 
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,57 +29,79 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/v1/book")
 public class RestBookController {
     private final BookService bookService;
-    private final PublisherService publisherService;
-    private final AuthorService authorService;
+    private final AuthorMapper authorMapper;
     private final BookMapper bookMapper;
+    private final PublisherMapper publisherMapper;
 
-    @GetMapping("/{isbn}")
-    public ResponseEntity<BookDTO> getBookByIsbn(@PathVariable String isbn) {
-        Book book = bookService.findBooksByIsbn(isbn);
-        return ResponseEntity.ok(bookMapper.toDTO(book));
+    @GetMapping("/title/{title}")
+    public ResponseEntity<List<BookDTO>> findBooksByTitle(@PathVariable String title) {
+        return ResponseEntity.ok(bookService.findBooksByTitle(title).stream().map(bookMapper::toDTO).collect(Collectors.toList()));
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Void> createBook(@Valid @RequestBody BookDTO bookDTO, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
+    @GetMapping("/isbn/{isbn}")
+    public ResponseEntity<Set<BookDTO>> findBooksByIsbn(@PathVariable String isbn) {
+        return ResponseEntity.ok(bookService.findBooksByIsbn(isbn).stream().map(bookMapper::toDTO).collect(Collectors.toSet()));
+    }
 
-        Publisher publisher = publisherService.findPublisherByName(bookDTO.getPublisherDTO().getName());
-        Set<Author> authors = bookDTO.getAuthors().stream()
-                .map(authorDTO -> authorService.findAuthorByName(authorDTO.getEmail()))
+    @PostMapping("/authors")
+    public ResponseEntity<List<BookDTO>> findBooksByAuthors(@RequestBody Set<AuthorDTO> authorDTOs) {
+        Set<Author> authors = authorDTOs.stream()
+                .map(authorMapper::toEntity)
                 .collect(Collectors.toSet());
+        return ResponseEntity.ok(bookService.findBooksByAuthors(authors).stream()
+                .map(bookMapper::toDTO)
+                .toList());
+    }
 
-        Book book = bookMapper.toEntity(bookDTO);
-        book.setPublisher(publisher);
-        book.setAuthors(authors);
+    @PostMapping("/publisher")
+    public ResponseEntity<List<BookDTO>> findBooksByPublisher(@RequestBody PublisherDTO publisherDTO) {
+        Publisher publisher = publisherMapper.toEntity(publisherDTO);
+        return ResponseEntity.ok(bookService.findBooksByPublishers(publisher).stream()
+                .map(bookMapper::toDTO)
+                .collect(Collectors.toList()));
+    }
 
-        bookService.createBook(book);
+//    @GetMapping("/{isbn}/authors")
+//    public ResponseEntity<List<AuthorDTO>> findAuthorsByBookIsbn(@PathVariable Long isbn) {
+//        List<Author> authors = bookService.findAuthorsByBookId(id);
+//        return ResponseEntity.ok(authors.stream()
+//                .map(authorMapper::toDto)
+//                .collect(Collectors.toList()));
+//    }
+//
+//    @GetMapping("/{isbn}/publisher")
+//    public ResponseEntity<PublisherDTO> findPublisherByBookIsbn(@PathVariable Long isbn) {
+//        Publisher publisher = bookService.findPublisherByBookId(id);
+//        return ResponseEntity.ok(publisherMapper.toDto(publisher));
+//    }
+
+    @PostMapping
+    public ResponseEntity<Void> createBook(@RequestBody @Valid BookDTO bookDTO) {
+        bookService.createBook(bookMapper.toEntity(bookDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateBookById(@PathVariable Long id, @RequestBody @Valid BookDTO bookDTO) {
+        bookService.updateBookById(id, bookMapper.toEntity(bookDTO));
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{isbn}")
-    public ResponseEntity<BookDTO> updateBookByIsbn(@PathVariable String isbn, @Valid @RequestBody BookDTO bookDTO, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        Publisher publisher = publisherService.findPublisherByName(bookDTO.getPublisherDTO().getName());
-        Set<Author> authors = bookDTO.getAuthors().stream()
-                .map(authorDTO -> authorService.findAuthorByName(authorDTO.getEmail()))
-                .collect(Collectors.toSet());
-
-        Book book = bookMapper.toEntity(bookDTO);
-        book.setPublisher(publisher);
-        book.setAuthors(authors);
-
-        bookService.updateBookByIsbn(isbn, book);
+    @PutMapping("/isbn/{isbn}")
+    public ResponseEntity<Void> updateBookByIsbn(@PathVariable String isbn, @RequestBody @Valid BookDTO bookDTO) {
+        bookService.updateBookByIsbn(isbn, bookMapper.toEntity(bookDTO));
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{isbn}")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBookById(@PathVariable Long id) {
+        bookService.deleteBookById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/isbn/{isbn}")
     public ResponseEntity<Void> deleteBookByIsbn(@PathVariable String isbn) {
         bookService.deleteBookByISBN(isbn);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
