3fae7f970a4f62266c064475633662b2cb0e6a6f
Stanislav Khytrych <khytrych@soft-xpansion.com.ua>
2024-09-01 17:00:47 +0300

    reimpl new publisher rest controller

--- src/main/java/tdr/pet/weblibrary/controller/RestPublisherController.java
+++ src/main/java/tdr/pet/weblibrary/controller/RestPublisherController.java
@@ -2,13 +2,19 @@ package tdr.pet.weblibrary.controller;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import tdr.pet.weblibrary.model.dto.PublisherDTO;
+import tdr.pet.weblibrary.model.entity.Publisher;
 import tdr.pet.weblibrary.model.mapper.PublisherMapper;
 import tdr.pet.weblibrary.service.PublisherService;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController("/api/v1/publisher")
 @AllArgsConstructor
@@ -16,37 +22,47 @@ public class RestPublisherController {
     private final PublisherService publisherService;
     private final PublisherMapper publisherMapper;
 
-    @GetMapping("/{publisherName}")
-    public ResponseEntity<PublisherDTO> getPublisherByName(@PathVariable String publisherName, BindingResult bindingResult) {
-        if (ObjectUtils.isEmpty(publisherName) || bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(publisherMapper.toDTO(publisherService.findPublisherByName(publisherName)));
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Set<PublisherDTO>> findPublishersByName(@PathVariable String name) {
+        return ResponseEntity.ok(publisherService.findPublishersByName(name).stream()
+                .map(publisherMapper::toDTO)
+                .collect(Collectors.toSet()));
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<PublisherDTO> createPublisher(@Valid @RequestBody PublisherDTO publisherDTO, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
+    @GetMapping("/address/{address}")
+    public ResponseEntity<List<PublisherDTO>> findPublishersByAddress(@PathVariable String address) {
+        return ResponseEntity.ok(publisherService.findPublishersByAddress(address).stream()
+                .map(publisherMapper::toDTO)
+                .collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createNewPublisher(@RequestBody @Valid PublisherDTO publisherDTO) {
         publisherService.createNewPublisher(publisherMapper.toEntity(publisherDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updatePublisherById(@PathVariable Long id, @RequestBody @Valid PublisherDTO publisherDTO) {
+        publisherService.updatePublisherById(id, publisherMapper.toEntity(publisherDTO));
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{publisherName}")
-    public ResponseEntity<PublisherDTO> updatePublisherByName(@PathVariable String publisherName, @Valid @RequestBody PublisherDTO publisherDTO, BindingResult bindingResult) {
-        if (ObjectUtils.isEmpty(publisherName) || bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
-        publisherService.updatePublisherByName(publisherName, publisherMapper.toEntity(publisherDTO));
+    @PutMapping("/name/{name}")
+    public ResponseEntity<Void> updatePublisherByName(@PathVariable String name, @RequestBody @Valid PublisherDTO publisherDTO) {
+        publisherService.updatePublisherByName(name, publisherMapper.toEntity(publisherDTO));
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{publisherName}")
-    public ResponseEntity<Void> deletePublisherByName(@PathVariable String publisherName, BindingResult bindingResult) {
-        if (ObjectUtils.isEmpty(publisherName) || bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok().build();
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePublisherById(@PathVariable Long id) {
+        publisherService.deletePublisherById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/name/{name}")
+    public ResponseEntity<Void> deletePublisherByName(@PathVariable String name) {
+        publisherService.deletePublisherByName(name);
+        return ResponseEntity.noContent().build();
     }
 }
